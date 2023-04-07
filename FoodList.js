b32ef320b4cbc72a19cde445a5c15bf17c858731
itsSharan13 <108917162+itsSharan13@users.noreply.github.com>
2023-04-07 16:16:58 +0530

    Create FoodList.js

---
+++ FoodList.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import TableRow from './TableRow';
+
+class FoodList extends Component {
+    constructor(props) {
+        super(props);
+      }
+      
+      tabRow(){
+        if(this.props.data instanceof Array){
+          return this.props.data.map(function(object, i){
+              return <TableRow obj={object} key={i} />;
+          })
+        }
+      }
+      render() {
+        return (
+            <div className="container">
+            <table className="table table-striped">
+              <thead>
+                <tr>
+                  <td>ID</td>
+                  <td>Name</td>
+                  <td>Price</td>
+                </tr>
+              </thead>
+              <tbody>
+                {this.tabRow()}
+              </tbody>
+            </table>
+        </div>
+        );
+      }
+}
+export default FoodList;
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Priv
